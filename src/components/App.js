dbe45f84e9adc3e9a4b8680dfc71a66d05864bf4
amank <aman.kaushik.975@gmail.com>
2024-06-25 19:18:44 +0530

    solution

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 
 import React from "react";
 import './../styles/App.css';
+import { useState } from "react";
 
 const App = () => {
+  const [name, setName] = useState("");
   return (
     <div>
-        {/* Do not remove the main div */}
+      <input type="text" placeholder="Enter your name:"  onChange={(e) => setName(e.target.value)}></input>
+      <p>{name ? `Hello ${name}!` : ""}</p>
     </div>
   )
 }
